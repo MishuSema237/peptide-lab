1a89ee6209e90db6355a52fce0cd2da725f6f8db
MishuSema237 <mishusema237@gmail.com>
2026-02-02 17:13:04 -0500

    new proxy

--- proxy.ts
+++ proxy.ts
@@ -25,8 +25,7 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-         * - api routes
          */
-        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)"
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
     ]
 };
