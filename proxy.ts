5ce945e952000b33b2e7fd077a139eb0a112054c
MishuSema237 <mishusema237@gmail.com>
2026-02-02 16:36:28 -0500

    middle ware bullshit

--- proxy.ts
+++ proxy.ts
@@ -25,7 +25,8 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         * - api routes
          */
-        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)"
     ]
 };
